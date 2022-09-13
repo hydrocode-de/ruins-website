0c21b3e8a4187bb162ff1f3f6f4caf137a963c57
Mirko Mälicke <mirko@hydrocode.de>
2022-09-13 16:57:24 +0200

    added transition to Header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,14 +1,27 @@
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import LangSwitch from "./LangSwitch";
 import QuickAccessMenu from "./QuickAccessMenu";
+import useScrollPosition from "./useScrollPosition";
 
-const AppHeader: React.FC = () => (
+const AppHeader: React.FC = () => {
+    // get the scroll position
+    const scrollPosition = useScrollPosition();
+    const startView = scrollPosition < 128;
+    
+    return (
         <Box sx={{flexGrow: 1}}>
-            <AppBar position="sticky" color="default" sx={{position: 'fixed'}}>
-                <Toolbar>
+            <AppBar position="sticky" color="default" sx={{
+                position: 'fixed',
+                backgroundColor: startView ? 'transparent' : 'white',
+                transition: '1s ease-in-out',
+                boxShadow: startView ? 'none' : '0px 2px 4px -1px rgba(0 ,0, 0, 0.2)'
+            }}>
+                <Toolbar sx={{height: startView ? '128px' : '64px', transition: '1s ease-in-out'}}>
+                   
                     <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        RUINS 
+                    {startView ? '' : 'Uncertainty matters'}
                     </Typography>
+                    
                     <Box>
                         <QuickAccessMenu />
                         <LangSwitch />
@@ -18,5 +31,6 @@ const AppHeader: React.FC = () => (
             </AppBar>
         </Box>
     )
+}
 
 export default AppHeader;
