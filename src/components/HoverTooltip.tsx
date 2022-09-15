957dd251b84830651478505766de9a43091a65e0
Mirko Mälicke <mirko.maelicke@kit.edu>
2022-09-15 09:52:55 +0200

    added external link option to Tooltip

--- src/components/HoverTooltip.tsx
+++ src/components/HoverTooltip.tsx
@@ -1,5 +1,5 @@
 import { Box, Card, CardMedia, IconButton, Popover, Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import { Close, LaunchOutlined } from '@mui/icons-material';
 import { useSelector } from "react-redux";
@@ -9,13 +9,15 @@ interface HoverTooltipProps {
     wikipedia?: string;
     img?: string;
     linkName?: string;
+    external?: string
 }
 
-const HoverTooltip: React.FC<React.PropsWithChildren<HoverTooltipProps>> = ({ children, wikipedia, img, linkName }) => {
+const HoverTooltip: React.FC<React.PropsWithChildren<HoverTooltipProps>> = ({ children, wikipedia, img, linkName, external }) => {
     // define the anchor element
     const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
     const [bodyText, setBodyText] = useState<any>('no text available');
     const [imgSrc, setImgSrc] = useState<string | null>(null);
+    const [link, setLink] = useState<string>('external link');
 
     // determine language settings
     const lang = useSelector((state: RootState) => state.settings.lang);
@@ -32,36 +34,42 @@ const HoverTooltip: React.FC<React.PropsWithChildren<HoverTooltipProps>> = ({ ch
     // determine open state
     const open = Boolean(anchorEl);
 
-    // get the linkName
-    let link: string = 'external link';
-    if (linkName) {
-        link = linkName;
-    }
+    useEffect(() => {
+        // get the linkName
+        if (linkName) setLink(linkName)
 
-    if (wikipedia) {
-        const apiUrl = `https://${lang}.wikipedia.org/api/rest_v1/page/summary/${wikipedia}`
-        axios.get(apiUrl).then(res => {
-            // get the summary
-            if (res.data.extract) {
-                setBodyText(res.data.extract);
+        if (wikipedia) {
+            const apiUrl = `https://${lang}.wikipedia.org/api/rest_v1/page/summary/${wikipedia}`
+            axios.get(apiUrl).then(res => {
+                // get the summary
+                if (res.data.extract) {
+                    setBodyText(res.data.extract);
+                } else {
+                    setBodyText('The wikipedia extract cannot be found')
+                }
+
+                // check if there is a img
+                if(res.data.originalimage?.source) {
+                    setImgSrc(res.data.originalimage.source);
+                }
+            });
+        } else {
+            let customBody: any = '';
+            if (children) {
+                customBody = children;
+            } else if (!external) {
+                customBody = 'No Tooltip info available';
             } else {
-                setBodyText('The wikipedia extract cannot be found')
+                customBody = lang === 'en' ? 'find further information following the link below' : 'Weiterführende Informationen können über den Link abgerufen werden'
             }
+            setBodyText(customBody);
+        }
 
-            // check if there is a img
-            if(res.data.originalimage?.source) {
-                setImgSrc(res.data.originalimage.source);
-            }
-        });
-    } else {
-        setBodyText(children ? children : 'No Tooltip info available')
-    }
-
-    // overwrite imgSrc if necessary
-    if (img) {
-        setImgSrc(img);
-    }
-
+        // overwrite imgSrc if necessary
+        if (img) {
+            setImgSrc(img);
+        }
+    }, []);
     // build the content
     let hoverBody: any;
 
@@ -75,6 +83,14 @@ const HoverTooltip: React.FC<React.PropsWithChildren<HoverTooltipProps>> = ({ ch
                     
                     { bodyText }
                 </Typography>
+                { external ? <>
+                        <Typography variant="caption" component="p" sx={{m: 'auto', p: 1}}>
+                            <a href={external} target="_blank">
+                                <LaunchOutlined fontSize="small" sx={{fontSize: '1rem', verticalAlign: 'middle'}} />
+                                { external.length > 50 ? external.slice(0, 47) + '...' : external }
+                            </a>
+                        </Typography>
+                    </> : null }
             </div>
         );
     } else if (bodyText && imgSrc ) {
@@ -87,12 +103,20 @@ const HoverTooltip: React.FC<React.PropsWithChildren<HoverTooltipProps>> = ({ ch
                     <Typography variant="caption" component="p" sx={{p: '1rem'}}>{ bodyText }</Typography>
                     { wikipedia ? <>
                     <Typography variant="caption" component="p" sx={{m: 'auto'}}>
-                        <a href={`https://${lang}.wikipedia.org/wiki/${wikipedia}`} target="blank">
+                        <a href={`https://${lang}.wikipedia.org/wiki/${wikipedia}`} target="_blank">
                             <LaunchOutlined fontSize="small" sx={{fontSize: '1rem', verticalAlign: 'middle'}} />
                             {lang==='en' ? 'source: Wikipedia page' : 'Quelle: Wikipedia Seite'}
                         </a>
                         </Typography>
                     </> : null}
+                    { external ? <>
+                        <Typography variant="caption" component="p" sx={{p: 1, m: 'auto'}}>
+                            <a href={external} target="_blank">
+                                <LaunchOutlined fontSize="small" sx={{fontSize: '1rem', verticalAlign: 'middle'}} />
+                                { external.length > 50 ? external.slice(0, 47) + '...' : external }
+                            </a>
+                        </Typography>
+                    </> : null }
                 </Box>
                 <CardMedia
                     component="img"
